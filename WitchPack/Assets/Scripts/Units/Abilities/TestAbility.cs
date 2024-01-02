88fdf7fe4486012f0627523e4d8af8879185f7df
On <ontamam@gmail.com>
2024-01-02 10:31:15 +0200

    projectiles

---
+++ WitchPack/Assets/Scripts/Units/Abilities/TestAbility.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAbility : OffensiveAbility
+{
+    // testing simple projectile 
+
+    public override void CastAbility(BaseUnit caster)
+    {
+        base.CastAbility(caster);
+
+    }
+
+}
---
+++ WitchPack/Assets/Scripts/Units/Projectile.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour
+{
+    private BaseAbility refAbility;
+    private BaseUnit owner;
+    [SerializeField] private int maxNumberOfHits;
+    [SerializeField] private float speed;
+    [SerializeField] private Rigidbody2D rb;
+    private int currentNumberOfHits;
+    public void Fire(BaseUnit shooter, BaseAbility givenAbility, Vector2 dir)
+    {
+        owner = shooter;
+        refAbility = givenAbility;
+        rb.velocity = dir * speed;
+        //rotate to look at dir given 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //projectiles can only hit the the layer of the opposite unit type
+       
+        BaseUnit target = collision.GetComponent<BaseUnit>();
+        if (!ReferenceEquals(target, null))
+        {
+            target.Damageable.GetHit(owner.DamageDealer, refAbility);
+        }
+        currentNumberOfHits++;
+        if (currentNumberOfHits >= maxNumberOfHits)
+        {
+            Disable();
+        }
+    }
+
+
+    private void Disable()
+    {
+        owner = null;
+        refAbility = null;
+        gameObject.SetActive(false);
+    }
+
+    private void OnValidate()
+    {
+        if (speed <= 1)
+        {
+            speed = 1;
+        }
+        if (maxNumberOfHits <= 1)
+        {
+            maxNumberOfHits = 1;
+        }
+    }
+
+}
