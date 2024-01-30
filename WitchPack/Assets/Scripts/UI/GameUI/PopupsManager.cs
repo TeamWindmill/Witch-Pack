7ae71d870993ea0fae5bd2369be442011dcfb4aa
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2024-01-30 14:03:10 +0200

    Sin for popups

--- WitchPack/Assets/Scripts/UI/GameUI/PopupsManager.cs
+++ WitchPack/Assets/Scripts/UI/GameUI/PopupsManager.cs
@@ -6,16 +6,27 @@ using DamageNumbersPro;
 public class PopupsManager : MonoBehaviour
 {
     [SerializeField] private DamageNumber _numberPrefab;
+    private float _randomOffset;
+    [Range(1, 100), SerializeField] private float _offsetMultiplier;
+    private float _offsetDivider = 100;
+    private Vector3 _offsetVector;
+
+    private void Update()
+    {
+        _randomOffset = Mathf.Sin(GAME_TIME.GameTime) * (_offsetMultiplier / _offsetDivider);
+        
+    }
 
     public DamageNumber NumberPrefab { get => _numberPrefab; }
 
     public void SpawnDamagePopup(Damageable damageable, DamageDealer damageDealer, DamageHandler damage, BaseAbility ability, bool isCrit)
     {
+        _offsetVector = new Vector3(_randomOffset, 0);
         Color numberPopupColor = Color.white;
         if (isCrit)
         {
             numberPopupColor = Color.yellow;
         }
-        NumberPrefab.Spawn(damageable.Owner.transform.position, damage.GetFinalDamage(), numberPopupColor);
+        NumberPrefab.Spawn(damageable.Owner.transform.position + _offsetVector, damage.GetFinalDamage(), numberPopupColor);
     }
 }
