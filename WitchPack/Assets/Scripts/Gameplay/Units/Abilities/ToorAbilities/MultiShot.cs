38b76819fa5c7b69d20e6588df0ec67c95fb17f5
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-03-12 16:32:30 +0200

    multishot basic working on 1 target

--- WitchPack/Assets/Scripts/Gameplay/Units/Abilities/ToorAbilities/MultiShot.cs
+++ WitchPack/Assets/Scripts/Gameplay/Units/Abilities/ToorAbilities/MultiShot.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class MultiShot : OffensiveAbility
 {
     [SerializeField] private int numberOfShots;
-    [SerializeField] private Vector3 offset;
+    [SerializeField] private int offset;
     [SerializeField] private int ricochetTimes; //how many times the bullet will bounce between targets
     [SerializeField] private float ricochetRange;
     [SerializeField] private float ricochetSpeed;
@@ -19,18 +19,19 @@ public class MultiShot : OffensiveAbility
                 MultiShotMono shotMono = LevelManager.Instance.PoolManager.MultiShotPool.GetPooledObject();
                 shotMono.transform.position = caster.CastPos.position;
                 shotMono.gameObject.SetActive(true);
-                Vector3 dir = target.transform.position - caster.transform.position;
+                var dir = target.transform.position - caster.transform.position;
+                var dirAngle = Vector3.Angle(Vector3.up, dir.normalized);
                 if (i == 0)
                 {
-                    shotMono.Init(target,this, Quaternion.Euler(dir).eulerAngles - new Vector3(0,0,-90));
+                    shotMono.Init(caster,target,this, dirAngle);
                 }
                 else if (i % 2 == 0)
                 {
-                    shotMono.Init(target,this,Quaternion.Euler(dir).eulerAngles + offset- new Vector3(0,0,-90));
+                    shotMono.Init(caster,target,this,dirAngle + offset);
                 }
                 else
                 {
-                    shotMono.Init(target,this,Quaternion.Euler(dir).eulerAngles - offset- new Vector3(0,0,-90));
+                    shotMono.Init(caster,target,this,dirAngle - offset);
                 }
             }
 
@@ -38,28 +39,6 @@ public class MultiShot : OffensiveAbility
         }
 
         return false;
-        // for (int i = 0; i < numberOfShots; i++)
-        // {
-        //     MultiShotMono shotMono = LevelManager.Instance.PoolManager.MultiShotPool.GetPooledObject();
-        //     shotMono.transform.position = caster.CastPos.position;
-        //     RicochetHandler richocheter = new RicochetHandler(shotMono, ricochetTimes, targetData, ricochetRange, ricochetSpeed);
-        //     shotMono.gameObject.SetActive(true);
-        //     Vector2 dir = foundTargets[i].transform.position - caster.transform.position;
-        //     if (i == 0)
-        //     {
-        //         shotMono.Fire(caster, this, dir.normalized, foundTargets[i], Vector3.zero);
-        //     }
-        //     else if (i % 2 == 0)
-        //     {
-        //         shotMono.Fire(caster, this, dir.normalized, foundTargets[i], offset);
-        //     }
-        //     else
-        //     {
-        //         shotMono.Fire(caster, this, dir.normalized, foundTargets[i], -offset);
-        //     }
-        // }
-        // return true;
-
     }
 
     public override bool CheckCastAvailable(BaseUnit caster)
@@ -69,6 +48,4 @@ public class MultiShot : OffensiveAbility
 
         return false;
     }
-
-
 }
--- WitchPack/Assets/Scripts/Gameplay/Units/Abilities/ToorAbilities/MultiShotMono.cs
+++ WitchPack/Assets/Scripts/Gameplay/Units/Abilities/ToorAbilities/MultiShotMono.cs
@@ -3,94 +3,60 @@ using System.Collections.Generic;
 using Tools.Helpers;
 using UnityEngine;
 
-public class MultiShotMono : InitializedMono<BaseUnit,OffensiveAbility,Vector3>
+public class MultiShotMono : MonoBehaviour
 {
-    [SerializeField] private int numOfPoint;
-    private Vector3 offset;
-    private const float DistanceToTarget = 1;
-    private Vector3 initialPosition;
-    private List<Vector3> allPositions;
-    private int counter = 0;
+    public bool Launched { get; private set; }
 
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private float _speed;
     [SerializeField] private float _curveSpeed;
+    [SerializeField] private float _Delay;
     private BaseUnit _target;
+    private Vector3 _targetPos;
+    private BaseUnit _caster;
+    private OffensiveAbility _ability;
 
-    public override void Init(BaseUnit target,OffensiveAbility offensiveAbility,Vector3 dir)
+    private static readonly float HIT_POS_OFFSET = 0.8f;
+
+    public void Init(BaseUnit caster, BaseUnit target,OffensiveAbility offensiveAbility,float angle)
     {
+        _caster = caster;
         _target = target;
-        transform.Rotate(Quaternion.Euler(dir).eulerAngles);
-        base.Init(target,offensiveAbility,dir);
+        _targetPos = target.transform.position;
+        _ability = offensiveAbility;
+        transform.rotation = Quaternion.Euler(0,0,angle);
+        TimerManager.Instance.AddTimer(_Delay, () => Launched = true, true);
     }
 
     private void FixedUpdate()
     {
-        if(!Initialized) return;
-
-        var dir = (Vector2)_target.transform.position - _rb.position;
-        dir.Normalize();
-        float rotateAmount = Vector3.Cross(dir,transform.forward).z;
-
-        _rb.angularVelocity = -rotateAmount * _curveSpeed;
-
         _rb.velocity = transform.up * _speed;
+        
+        if(!Launched) return;
+        if(Vector3.Distance(transform.position, _targetPos) < HIT_POS_OFFSET) Disable();
+        var dir = _rb.position - (Vector2)_targetPos;
+        dir.Normalize();
+        float rotateAmount = Vector3.Cross(dir,transform.up).z;
+
+        _rb.angularVelocity = rotateAmount * _curveSpeed;
     }
-    
+
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        // BaseUnit target = collision.GetComponent<BaseUnit>();
-        // if (!ReferenceEquals(target, null) && ReferenceEquals(target, this.target))
-        // {
-        //     target.Damageable.GetHit(owner.DamageDealer, ability);
-        //     OnShotHit?.Invoke(ability, owner, target);
-        //     Disable();
-        // }
+        BaseUnit target = collision.GetComponent<BaseUnit>();
+        if (!ReferenceEquals(target, null) && ReferenceEquals(target, _target))
+        {
+            target.Damageable.GetHit(_caster.DamageDealer, _ability);
+            Disable();
+        }
     }
-    public virtual void Disable()
+    private void Disable()
     {
-        // owner = null;
-        // ability = null;
-        // SetSpeed(initialSpeed);
-        // OnShotHit?.RemoveAllListeners();
-        // gameObject.SetActive(false);
-    }
-    // public void Fire(BaseUnit shooter, OffensiveAbility givenAbility, Vector2 dir, BaseUnit target, Vector3 archSize)
-    // {
-    //     owner = shooter;
-    //     ability = givenAbility;
-    //     this.target = target;
-    //     offset = archSize;
-    //     if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
-    //     {
-    //         offset = new Vector2(0, offset.y * 2);
-    //     }
-    //     else
-    //     {
-    //         offset = new Vector2(offset.x * 2, 0);
-    //     }
-    //     Rotate(dir);
-    //     Init();
-    // }
-    //
-    // private void Init()
-    // {
-    //     initialPosition = transform.position;
-    //     allPositions = new List<Vector3>(numOfPoint);
-    //     Initiallized = true;
-    //
-    //     for (var i = 0; i < numOfPoint; i++)
-    //     {
-    //         var newPosition = CubicCurve(initialPosition, initialPosition + offset, initialPosition + offset,
-    //             target.transform.position, (float)i / numOfPoint);
-    //         allPositions.Add(newPosition);
-    //     }
-    //     Initiallized = true;
-    // }
-    
-    protected void Rotate(Vector2 dir)
-    {
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        _caster = null;
+        _ability = null;
+        _target = null;
+        Launched = false;
+        gameObject.SetActive(false);
     }
 }
