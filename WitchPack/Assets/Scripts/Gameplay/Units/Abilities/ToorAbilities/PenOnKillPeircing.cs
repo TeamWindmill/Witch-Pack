5aab3782bd5d00e4c77af47e0a1971cce2475c16
OhMyCodeIsTaken <ronbandelmusic@gmail.com>
2024-03-13 18:55:31 +0200

    Added Pierce Level Up popup

--- WitchPack/Assets/Scripts/Gameplay/Units/Abilities/ToorAbilities/PenOnKillPeircing.cs
+++ WitchPack/Assets/Scripts/Gameplay/Units/Abilities/ToorAbilities/PenOnKillPeircing.cs
@@ -8,6 +8,7 @@ public class PenOnKillPeircing : PiercingShot
 {
     [SerializeField] private int extraPenPerKill;
     [SerializeField] private int numberOfKillsRequiredToIncreasePierce;
+    [SerializeField] private Color popupColor;
 
     public override void OnSetCaster(BaseUnit caster)
     {
@@ -19,5 +20,9 @@ public class PenOnKillPeircing : PiercingShot
     private void IncreasePen(AbilityEventCounter counter, Damageable target, DamageDealer dealer, DamageHandler dmg, BaseAbility ability)
     {
         dealer.Owner.Stats.AddValueToStat(StatType.AbilityProjectilePenetration, extraPenPerKill);
+        // Play Sound
+        SoundManager.Instance.PlayAudioClip(SoundEffectType.ExperiencedHunterLevelUp);
+        // Popup
+        LevelManager.Instance.PopupsManager.SpawnGeneralPopup("PIERCE UP+", popupColor, dealer.Owner.transform.position, yOffset: true);
     }
 }
