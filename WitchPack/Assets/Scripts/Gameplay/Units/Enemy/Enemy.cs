d0b26687c9f36cefe0d918f79028304a1e1fbfcd
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-05-01 14:18:56 +0300

    enemy dynamic size, increased camera zoom

--- WitchPack/Assets/Scripts/Gameplay/Units/Enemy/Enemy.cs
+++ WitchPack/Assets/Scripts/Gameplay/Units/Enemy/Enemy.cs
@@ -38,6 +38,7 @@ public class Enemy : BaseUnit
         pointIndex = 0;
         enemyConfig = givenConfig as EnemyConfig;
         base.Init(enemyConfig);
+        transform.localScale = new Vector3(enemyConfig.Size,enemyConfig.Size,enemyConfig.Size);
         _path = enemyConfig.Path;
         _coreDamage = enemyConfig.CoreDamage;
         _energyPoints = enemyConfig.EnergyPoints;
