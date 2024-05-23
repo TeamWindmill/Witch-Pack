14c88ef8decd1134007f49ec8a614928d9466f04
IdoFolk <idof0321@gmail.com>
2024-05-23 20:16:19 +0300

    .

--- WitchPack/Assets/Scripts/UI/AbilityMetaUpgrades/AbilityMetaUpgradeIcon.cs
+++ WitchPack/Assets/Scripts/UI/AbilityMetaUpgrades/AbilityMetaUpgradeIcon.cs
@@ -3,13 +3,20 @@ using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class AbilityMetaUpgradeIcon : ClickableUIElement
 {
     [SerializeField] private AbilityMetaUpgradeIcon childNode;
     [SerializeField] private TextMeshProUGUI _name;
     [SerializeField] private TextMeshProUGUI _amount;
-
+    [SerializeField] private Image _lineImage;
+    [SerializeField] private Image _frameImage;
+    [Space] 
+    [BoxGroup("Sprites")][SerializeField] private Sprite upgradeReadyFrameSprite;
+    [BoxGroup("Sprites")][SerializeField] private Sprite defaultFrameSprite;
+    [BoxGroup("Sprites")][SerializeField] private Sprite defaultLineSprite;
+    [BoxGroup("Sprites")][SerializeField] private Sprite upgradedLineSprite;
     public AbilityUpgradeState UpgradeState { get; private set; } = AbilityUpgradeState.Locked;
 
     public void Init(AbilityStatUpgrade abilityUpgrade)
@@ -29,10 +36,26 @@ public class AbilityMetaUpgradeIcon : ClickableUIElement
             case AbilityUpgradeState.Open:
                 UpgradeState = AbilityUpgradeState.Upgraded;
                 childNode.UpgradeState = AbilityUpgradeState.Open;
+                
                 break;
             case AbilityUpgradeState.Upgraded:
                 break;
         }
         base.OnClick(eventData);
+    }
+
+    private void ChangeState(AbilityUpgradeState upgradeState)
+    {
+        switch (upgradeState)
+        {
+            case AbilityUpgradeState.Locked:
+                break;
+            case AbilityUpgradeState.Open:
+                break;
+            case AbilityUpgradeState.Upgraded:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(upgradeState), upgradeState, null);
+        }
     }
 }
