7ae633031f6fa7845ce265b1054dd5c7ed479e8b
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-06-16 13:39:12 +0300

    Dialog Node

--- WitchPack/Assets/Scripts/Map/DialogNode.cs
+++ WitchPack/Assets/Scripts/Map/DialogNode.cs
@@ -1,3 +1,18 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
 public class DialogNode : MapNode
 {
+    [BoxGroup("Dialog")][SerializeField] private DialogSequence _dialogConfig;
+    protected override void OnNodeClick(PointerEventData.InputButton button)
+    {
+        DialogBox.Instance.SetDialogSequence(_dialogConfig,FinishDialog);
+        base.OnNodeClick(button);
+    }
+
+    private void FinishDialog()
+    {
+        //change state to complete
+    }
 }
