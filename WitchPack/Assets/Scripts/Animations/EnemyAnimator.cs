9688b85c570bd7a242461fe0fe5c8ce00e4b2b2e
IdoFolk <121632753+IdoFolk@users.noreply.github.com>
2024-07-23 18:53:46 +0300

    removed enemy size temporary, fixed poison ivy timer bug

--- WitchPack/Assets/Scripts/Animations/EnemyAnimator.cs
+++ WitchPack/Assets/Scripts/Animations/EnemyAnimator.cs
@@ -7,6 +7,7 @@ public class EnemyAnimator : UnitAnimator
     {
         base.Init(unit);
         _enemy = unit as Enemy;
+        animator.Play("Idle");
     }
 
     protected override void MoveAnimation()
--- WitchPack/Assets/Scripts/Configs/EnemyConfig.cs
+++ WitchPack/Assets/Scripts/Configs/EnemyConfig.cs
@@ -12,9 +12,9 @@ public class EnemyConfig : BaseUnitConfig
     [BoxGroup("Enemy")][SerializeField] private int energyPoints;
     [BoxGroup("Enemy")][SerializeField] private List<AbilitySO> _abilities;
     [BoxGroup("Enemy")][SerializeField] private EnemyAIConfig _enemyAIConfig;
-    [BoxGroup("Enemy")] [SerializeField] private float _size;
+    //[BoxGroup("Enemy")] [SerializeField] private float _size;
 
-    public float Size => _size;
+    //public float Size => _size;
     public EnemyAIConfig EnemyAIConfig => _enemyAIConfig;
     public int CoreDamage => coreDamage;
     public int EnergyPoints => energyPoints;
